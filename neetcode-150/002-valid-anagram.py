44e6710a7e56ea03537000520c72c5f62c6fbd25
Edgar Chirivella <edgar.95.12@gmail.com>
2025-03-12 21:25:43 +0000

    Neetcode 2: Valid Anagram

---
+++ neetcode-150/002-valid-anagram.py
@@ -0,0 +1,14 @@
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+        countS[s[i]] = countS.get(s[i], 0) + 1
+        countT[t[i]] = countT.get(t[i], 0) + 1
+
+    return countS == countT
+
+
+print(isAnagram("racecar", "carrace"))
